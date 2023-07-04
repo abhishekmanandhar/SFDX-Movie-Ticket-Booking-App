6d88a62c71a3ed1ca24c7972e673e4d6da9a7e00
abhishekmanandhar <csit180802_abhishek@achsnepal.edu.np>
2023-07-04 16:41:33 +0545

    integrated payment feature after the user books the ticket

--- force-app/main/default/lwc/bookingDetails/bookingDetails.js
+++ force-app/main/default/lwc/bookingDetails/bookingDetails.js
@@ -2,6 +2,11 @@ import { LightningElement, api, wire } from 'lwc';
 import getBookingDetails from '@salesforce/apex/BookingController.getBookingDetails';
 import getMovieDetails from '@salesforce/apex/MovieController.getMovieDetails';
 import getShowtimeDetails from '@salesforce/apex/ShowtimeController.getShowtimeDetails';
+import { createRecord } from 'lightning/uiRecordApi';
+import PAYMENT_OBJECT from '@salesforce/schema/Payment__c';
+import BOOKING_FIELD from '@salesforce/schema/Payment__c.Booking__c';
+import PAYMENT_TYPE_FIELD from '@salesforce/schema/Payment__c.Payment_Type__c';
+import TOTAL_PAYABLE_AMOUNT_FIELD from '@salesforce/schema/Payment__c.Total_Payable_Amount__c';
 
 export default class BookingDetails extends LightningElement {
     @api bookingId;
@@ -21,6 +26,16 @@ export default class BookingDetails extends LightningElement {
     bookingDetails;
     movieDetails;
     showtimeDetails;
+
+    get paymentTypes() {
+        return [
+            { label: 'Credit/Debit Card', value: 'Credit/Debit Card' },
+            { label: 'Cash On Delivery', value: 'Cash On Delivery' },
+        ];
+    }
+
+    selectedPaymentType = '';
+    totalPayableAmount;
 
     @wire(getBookingDetails, { bookingId: '$bookingId' })
     wiredBookingDetails({ error, data }) {
@@ -64,6 +79,27 @@ export default class BookingDetails extends LightningElement {
         }
     }
 
+    handlePaymentTypeChange(event){
+        this.selectedPaymentType = event.target.value;
+        console.log(this.selectedPaymentType);
+    }
+
+    handlePayNow(){
+        const fields = {};
+        fields[BOOKING_FIELD.fieldApiName] = this.bookingId;
+        fields[TOTAL_PAYABLE_AMOUNT_FIELD.fieldApiName] = this.totalPayableAmount;
+        fields[PAYMENT_TYPE_FIELD.fieldApiName] = this.selectedPaymentType;
+
+        const paymentRecord = { apiName: PAYMENT_OBJECT.objectApiName, fields };
+        createRecord(paymentRecord)
+        .then(result => {
+            console.log('success')
+        })
+        .catch(error => {
+            console.error(error)
+        });
+    }
+
     get formattedStartTime() {
         if (this.starttime) {
             const options = {
@@ -79,4 +115,30 @@ export default class BookingDetails extends LightningElement {
         }
         return '';
     }
+
+    get calculateDiscountAmount() {
+        const discountPercentageSeats = 10; // Discount percentage for the number of seats greater >= 4
+        const discountPercentageSaturday = 50; // Discount percentage for Saturday
+    
+        let discountAmount = 0;
+    
+        // Apply discount based on the number of seats
+        if (this.numberOfSeats >= 4) {
+            const seatsDiscount = (this.totalPrice * discountPercentageSeats) / 100;
+            discountAmount += seatsDiscount;
+        }
+    
+        // Apply discount if it's Saturday
+        // const startDate = new Date(this.starttime);
+        // const isSaturday = startDate.getDay() === 6;
+        // if (isSaturday) {
+        //     const saturdayDiscount = (this.totalPrice * discountPercentageSaturday) / 100;
+        //     discountAmount += saturdayDiscount;
+        // }
+    
+        this.totalPayableAmount = this.totalPrice - discountAmount;
+
+        return discountAmount;
+    }
+    
 }
---
+++ force-app/main/default/triggers/PaymentTrigger.trigger
@@ -0,0 +1,29 @@
+trigger PaymentTrigger on Payment__c (after insert) {
+    if(Trigger.isAfter){
+        if(Trigger.isInsert){
+            Set<Id> bookingIds = new Set<Id>();
+
+            for (Payment__c payment : Trigger.new) {
+                if (payment.Booking__c != null) {
+                    bookingIds.add(payment.Booking__c);
+                }
+            }
+
+            List<Booking__c> bookingsToUpdate = [SELECT Id, Seat_Assignment_Id__c FROM Booking__c WHERE Id IN :bookingIds];
+
+            List<Seat_Assignment__c> seatAssignmentsToUpdate = new List<Seat_Assignment__c>();
+            for (Booking__c booking : bookingsToUpdate) {
+                booking.Booking_Status__c = 'Confirmed';
+                String[] seatAssignmentIds = booking.Seat_Assignment_Id__c.split(',');
+                for (String seatAssignmentId : seatAssignmentIds) {
+                    seatAssignmentsToUpdate.add(new Seat_Assignment__c(Id = seatAssignmentId, Availability_Status__c = 'Reserved'));
+                }
+            }
+
+            if(!bookingsToUpdate.isEmpty() && !seatAssignmentsToUpdate.isEmpty()){
+                update bookingsToUpdate;
+                update seatAssignmentsToUpdate;
+            }
+        }
+    }
+}
